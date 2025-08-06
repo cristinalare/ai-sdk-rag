b2a07b7f3d75869da538c25d5c3e344ad265d6d6
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2025-08-06 14:02:57 +0300

    feat: add tools

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,7 @@
+import { createResource } from "@/lib/actions/resources";
 import { openai } from "@ai-sdk/openai";
-import { convertToModelMessages, streamText, UIMessage } from "ai";
+import { convertToModelMessages, streamText, tool, UIMessage } from "ai";
+import z from "zod";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -8,8 +10,24 @@ export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-    model: openai("gpt-4o"),
+    model: openai("gpt-4o-mini"),
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
     messages: convertToModelMessages(messages),
+    tools: {
+      web_search_preview: openai.tools.webSearchPreview({}),
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        inputSchema: z.object({
+          content: z
+            .string()
+            .describe("the content or resource to add to the knowledge base"),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+    },
   });
 
   return result.toUIMessageStreamResponse();
