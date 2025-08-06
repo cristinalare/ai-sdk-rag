fdb18097725716db140df78cd605ba39ede7a7c1
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2025-08-06 15:51:26 +0300

    feat: add getInformation

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,5 @@
 import { createResource } from "@/lib/actions/resources";
+import { findRelevantContent } from "@/lib/ai/embedding";
 import { openai } from "@ai-sdk/openai";
 import { convertToModelMessages, stepCountIs, streamText, tool, UIMessage } from "ai";
 import z from "zod";
@@ -17,7 +18,7 @@ export async function POST(req: Request) {
     messages: convertToModelMessages(messages),
     stopWhen: stepCountIs(5),
     tools: {
-      web_search_preview: openai.tools.webSearchPreview({}),
+      // web_search_preview: openai.tools.webSearchPreview({}),
       addResource: tool({
         description: `add a resource to your knowledge base.
           If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
@@ -27,6 +28,13 @@ export async function POST(req: Request) {
             .describe("the content or resource to add to the knowledge base"),
         }),
         execute: async ({ content }) => createResource({ content }),
+      }),
+      getInformation: tool({
+        description: `get information from your knowledge base to answer questions.`,
+        inputSchema: z.object({
+          question: z.string().describe("the users question"),
+        }),
+        execute: async ({ question }) => findRelevantContent(question),
       }),
     },
   });
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 export default function Chat() {
   const [input, setInput] = useState("");
   const { messages, sendMessage } = useChat();
-  
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       <div className="space-y-4">
@@ -14,10 +14,21 @@ export default function Chat() {
           <div key={m.id} className="whitespace-pre-wrap">
             <div>
               <div className="font-bold">{m.role}</div>
-              {m.parts.map((part) => {
+              {m.parts.map((part, i) => {
                 switch (part.type) {
                   case "text":
-                    return <p>{part.text}</p>;
+                    return <p key={`${m.id}-part-${i}`}>{part.text}</p>;
+                  case "tool-addResource":
+                  case "tool-getInformation":
+                    return (
+                      <p key={`${m.id}-part-${i}`}>
+                        call{part.state === "output-available" ? "ed" : "ing"}{" "}
+                        tool: {part.type}
+                        <pre className="my-4 bg-zinc-100 p-2 rounded-sm">
+                          {JSON.stringify(part.input, null, 2)}
+                        </pre>
+                      </p>
+                    );
                 }
               })}
             </div>
